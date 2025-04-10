3ff5069c5edef41230d55c0b1a70984c334fc40f
Avinash Chaturvedi <avinashchaturvedi2002@gmail.com>
2025-04-11 02:13:12 +0530

    http replaced with https

--- frontend/src/components/WeatherCard.jsx
+++ frontend/src/components/WeatherCard.jsx
@@ -29,7 +29,7 @@ export default function WeatherCard(){
   async function getCityPos() {
     try {
       const response = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${WEATHER_API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${WEATHER_API_KEY}`
       );
       if (response.data && response.data.length > 0) {
         const { lat, lon } = response.data[0];
@@ -93,7 +93,7 @@ export default function WeatherCard(){
       async function fetchWeather() {
         try {
           const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${WEATHER_API_KEY}`
+            `httpss://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${WEATHER_API_KEY}`
           );
           setWeatherData(response.data);
         } catch (error) {
@@ -111,7 +111,7 @@ export default function WeatherCard(){
 useEffect(() => {
   if (weatherData) {
     const icon = weatherData.weather[0].icon;
-    setIconUrl(`https://openweathermap.org/img/wn/${icon}@2x.png`);
+    setIconUrl(`httpss://openweathermap.org/img/wn/${icon}@2x.png`);
     console.log(weatherData);
   }
 }, [weatherData]);
@@ -125,7 +125,7 @@ useEffect(() => {
        
         console.log(cityName);
         const unsplashResponse = await axios.get(
-          `https://api.unsplash.com/search/photos?page=1&query=${weatherData.name}&client_id=${UNSPLASH_API_KEY}`
+          `httpss://api.unsplash.com/search/photos?page=1&query=${weatherData.name}&client_id=${UNSPLASH_API_KEY}`
         );
   
         const firstPhoto = unsplashResponse.data.results[0];
@@ -170,12 +170,12 @@ useEffect(() => {
         <input type="email" placeholder="Search City..."
           class="w-full outline-none bg-black/5 text-white text-sm px-4 py-3 opacity-100 " onChange={handleCity}/>
           <button type="button" class=" inset-y-0 end-0 flex items-center justify-center p-3 bg-[#007bff] border-x border-x-white" onClick={startListening}>
-            <svg class="w-4 h-4  text-gray-500 dark:text-white hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+            <svg class="w-4 h-4  text-gray-500 dark:text-white hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"/>
             </svg>
         </button>
         <button type='button' class="flex items-center justify-center bg-[#007bff] px-5" onClick={getCityPos}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-white">
+          <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-white">
             <path
               d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
             </path>
